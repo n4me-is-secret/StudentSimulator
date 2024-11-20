9524df08505bc64e43b14ef8206a9b56da0894a8
n4me-is-secret <ttrinicin@gmail.com>
2024-11-20 10:52:55 +1000

    Небольшие изменения
    Создал загрузочный экран, подкорректировал анимации персонажей и их управление, создали префабы

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    
+    private Animator anim;
     private Rigidbody2D rb;
     private Vector2 moveVector;
-
     [SerializeField] private float speed = 4f;
+    [SerializeField] private GameObject playerVisuality;
 
     void Awake()
     {
 
+        anim = playerVisuality.GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         moveVector = new Vector2();
 
@@ -22,6 +23,14 @@ public class MovePlayer : MonoBehaviour
         {
             moveVector.x = Input.GetAxis("Horizontal");
             moveVector.y = Input.GetAxis("Vertical");
+
+            if (moveVector.x != 0 || moveVector.y != 0) anim.SetBool("isMoving", true);
+            else anim.SetBool("isMoving", false);
+
+            if (moveVector.x > 0) transform.localScale = new Vector2(1, 1);
+            if (moveVector.x < 0) transform.localScale = new Vector2(-1, 1);
+
+
             rb.MovePosition(rb.position + moveVector * speed * Time.fixedDeltaTime);
         }
 
